78fd6cc54a9a6b71ce3fd6f17a0d51df069b027a
AndreyPushkinA <57715810+AndreyPushkinA@users.noreply.github.com>
2023-08-28 16:29:27 +0300

    Update webhook.py

---
+++ webhook.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+import subprocess
+import os
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+
+    event = request.headers.get('X-GitHub-Event')
+    if event == 'push':
+        try:
+            playbook_path = "/opt/webhook/playbook.yml"
+            
+            subprocess.run(["ansible-playbook", playbook_path])
+            print("Ansible playbook executed successfully")
+        except Exception as e:
+            print("Error executing Ansible playbook:", e)
+
+    return "Webhook received successfully"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
